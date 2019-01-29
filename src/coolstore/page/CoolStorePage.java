d8820fff0b06090374009e21bb75f8d85b5e27a4
silkies <greenmiracle123@gmail.com>
2019-01-29 10:31:41 +0100

    Updated page

--- src/coolstore/page/CoolStorePage.java
+++ src/coolstore/page/CoolStorePage.java
@@ -29,12 +29,12 @@ public class CoolStorePage {
 	public void goToHomePage() throws InterruptedException {
         this.driver.get("http://web-ui-coolstore-prod-demo.apps.s-und-n.de");
         System.out.println("Browser launched and navigated to CoolStore page");
-        Thread.sleep(10000);
+        //Thread.sleep(10000);
 
 }
 	
 	public List<String> getListOfItems() throws InterruptedException {
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		return driver.findElements(By.xpath("//div[@ng-repeat='item in products']"))
                       .stream()
                       .map(this::getItemAndPrice)
@@ -66,13 +66,6 @@ public class CoolStorePage {
 		return price.substring(1);
 	}
 	
-	/*public void addAllItemsToCart() {
-
-		for (WebElement el : driver.findElements(By.cssSelector(".card-pf.card-pf-accented"))) {
-			clickAddToCart();
-		}
-		
-	}*/
 	
 	public Double sumOfAllPrices() throws InterruptedException {
 		double sum = 0;
@@ -80,7 +73,7 @@ public class CoolStorePage {
 		for (WebElement el : driver.findElements(By.xpath("//div[@ng-repeat='item in products']"))) {
 			sum += Double.parseDouble(getPrice(el));
 		}
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		Double truncatedDouble = BigDecimal.valueOf(sum)
 			    .setScale(2, RoundingMode.HALF_UP)
 			    .doubleValue();
@@ -88,27 +81,22 @@ public class CoolStorePage {
 	}
 	
 	public void addAllItemsToCart() throws InterruptedException {
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		List<WebElement> buttons = driver.findElements(By.xpath("//button[contains(text(),'Add To Cart')]"));
 
 		for(int i=0;i<buttons.size();i++){
 		    buttons.get(i).click();
 			System.out.println("Add to cart CLICKED");
-			Thread.sleep(3000);
+			//Thread.sleep(3000);
 		}
 		
-		//System.out.println(driver.findElements(By.xpath("//button[contains(text(),'Add To Cart')]")));
-		/*for( WebElement e :driver.findElements(By.xpath("//button[contains(text(),'Add To Cart')]"))) {
-			e.click();
-			System.out.println("CLICKED");
-		}*/
 		
 	}
 	
 	public void goToCart() throws InterruptedException {
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		driver.findElement(By.xpath("//a[@ng-href='#/cart']")).click();
-		Thread.sleep(10000);
+		//Thread.sleep(10000);
 		
 	}
 	
